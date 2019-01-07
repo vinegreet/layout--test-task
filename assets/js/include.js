af2e6a1f2914f3b1defcb22b8e505a66c7eef2fd
vinegreet <vinegreet@gmail.com>
2019-01-08 01:52:30 +0200

    Add header, jumbotron section. Write html for services and reviews sections

--- assets/js/include.js
+++ assets/js/include.js
@@ -1,6 +1,34 @@
 
 jQuery(document).ready(function($){
 
+  $('.header-hamburgerBtn').click(function() {
+    $(this).find('.header-hamburgerSlider').toggleClass('header-hamburgerSlider--active');
+    $('.header-menuDropdown').toggleClass('header-menuDropdown--open');
+  });
+  
+  $('.header-langToggle').click(function() {
+    $(this).toggleClass('header-langToggle--right');
+  });
 
+  $('.jumbo-playBtn').click(function() {
+    $(this).remove();
+  });
+
+  let ticking;
+
+  $(window).on('scroll', function(e) {
+    if (!ticking) {
+      const scroll = $(this).scrollTop();
+      requestAnimationFrame(function() {
+        if (scroll) {
+          $('.header').addClass('header--shadow');
+        } else {
+          $('.header').removeClass('header--shadow');
+        }
+        ticking = false;
+      });
+    }
+    ticking = true;
+  });
 
 });
