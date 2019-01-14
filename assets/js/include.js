92dd436b9d3a8a752aa619db69ef9e1dc3310331
vinegreet <vinegreet@gmail.com>
2019-01-14 02:38:23 +0200

    Add footer and modals

--- assets/js/include.js
+++ assets/js/include.js
@@ -1,5 +1,6 @@
 
 jQuery(document).ready(function($){
+  const isMobile = $(window).width() < 768;
 
   // ------------ Header toggles ------------
 
@@ -14,17 +15,26 @@ jQuery(document).ready(function($){
 
   // ------------ Header on scroll ------------
 
+  const $window = $(window);
   const $header = $('.header');
-  let ticking;
+  let scrollTop = $window.scrollTop();
+  let ticking, hasClass;
 
-  $(window).on('scroll', function() {
+  if (scrollTop) {
+    $header.addClass('header--shadow');
+    hasClass = true;
+  }
+
+  $window.on('scroll', function() {
     if (!ticking) {
-      const scroll = $(this).scrollTop();
+      scrollTop = $window.scrollTop();
       requestAnimationFrame(function() {
-        if (scroll) {
+        if (scrollTop && !hasClass) {
           $header.addClass('header--shadow');
-        } else {
+          hasClass = true;
+        } else if (!scrollTop && hasClass) {
           $header.removeClass('header--shadow');
+          hasClass = false;
         }
         ticking = false;
       });
@@ -32,18 +42,48 @@ jQuery(document).ready(function($){
     ticking = true;
   });
 
-  // ------------ Jumbotron play button  ------------
+  // ------------ Jumbotron -- Play button  ------------
 
   $('.jumbo-playBtn').click(function() {
     $(this).toggleClass('jumbo-playBtn--paused');
   });
 
-  // ------------ Dropdown ------------
+  // ------------ Services -- Modal elements ------------
+
+  const $servicesItem = $('.services-item');
+  const $servicesModal = $('.services-modal');
+
+  $servicesItem.each((idx, $item) => {
+    const isEven = idx % 2 === 0;
+    new Tether({
+      element: $servicesModal.eq(idx),
+      target: $item,
+      attachment: isMobile? 'top left' :
+        isEven? 'top left' : 'top right',
+      targetAttachment: isMobile? 'bottom left' :
+        isEven? 'top right' : 'top left'
+    });
+  });
+
+  $servicesModal.hide();
+
+  $servicesItem.click(function() {
+    const thisIdx = $servicesItem.index(this);
+    const $thisModal = $servicesModal.eq(thisIdx);
+    if ($thisModal.is(':visible')) {
+      $thisModal.hide(250);
+      return;
+    }
+    $servicesModal.hide();
+    $thisModal.show(250);
+  });
+
+
+  // ------------ Contact -- Dropdown ------------
 
   const $dropdown = $('.contact-dropdown');
   const $multiselect =  $('.contact-multiselect');
   const multiselectHeight = $multiselect[0].offsetHeight;
-  const isMobile = $(window).width() < 768;
   let isDropdownSqueezed;
 
   $('.contact-dropdownHeader').click(function(e) {
@@ -58,19 +98,30 @@ jQuery(document).ready(function($){
 
   // ------------ Miltiselect in Contact ------------
 
-  $('.contact-select').click(function() {
+  const $contactSelect = $('.contact-select');
+  const $contactSelectDefinition = $('.contact-selectDefinition');
+
+  $contactSelect.click(function() {
     const $this = $(this);
     $this.find('.contact-selectInput')[0].checked = !$this.find('.contact-selectInput')[0].checked;
     $this.toggleClass('contact-select--checked');
     $this.next('.contact-selectDefinition').toggleClass('contact-selectDefinition--checked');
   });
   
-  $('.contact-selectDefinition').click(function() {
+  $contactSelectDefinition.click(function() {
     const $this = $(this);
     const $select = $this.prev('.contact-select');
     $select.find('.contact-selectInput')[0].checked = !$select.find('.contact-selectInput')[0].checked;
     $select.toggleClass('contact-select--checked');
     $this.toggleClass('contact-selectDefinition--checked');
+  });
+
+  $contactSelect.on('mouseover mouseleave', function() {
+    $(this).next('.contact-selectDefinition').toggleClass('contact-selectDefinition--hover');    
+  });
+  
+  $contactSelectDefinition.on('mouseover mouseleave', function() {
+    $(this).prev('.contact-select').toggleClass('contact-select--hover');
   });
 
 });
