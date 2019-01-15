b3bcf07c4d92a0acb1c8e154501ef27deb8b4b7e
vinegreet <vinegreet@gmail.com>
2019-01-15 03:42:07 +0200

    Add menu buttons functionality, icons change on hover, social networks SVGs, dotted lines in Services section; Make fixes

--- assets/js/include.js
+++ assets/js/include.js
@@ -13,29 +13,89 @@ jQuery(document).ready(function($){
     $(this).toggleClass('header-langToggle--right');
   });
 
+  // ------------ Menu scroll ------------
+
+  const $htmlAndBody = $('html, body');
+  const headerHeightWithCorrection = 66;
+  const $buttons = $('.header-menuBtn, .jumbo-btn');
+
+  $buttons.click(function() {
+    const $this = $(this);
+    const scrollTo = $($this.data('scroll-to')).offset().top;
+    $htmlAndBody.animate({ scrollTop: scrollTo - headerHeightWithCorrection }, 250);
+  });
+
+  $('.header-logoBtn').click(function() {
+    $htmlAndBody.animate({ scrollTop: 0 }, 250);
+  });
+
   // ------------ Header on scroll ------------
+
+  // --- Menu buttons highlighting  ---
+
+  const $menuBtn = $('.header-menuBtn');
+  const sectionsOffsets = [];
+  const menuBtnHasClass = new Array(3).fill(false);
+  const headerHeight = 71;
+  let classesDeleted;
+
+  $('section:not(.jumbo)').each(function(idx, $section) {
+    const offset = $section.offsetTop;
+    sectionsOffsets.push(offset);
+  });
+
+  function currentSection(scrollTop) {
+    const sectionIdx = sectionsOffsets.findIndex(offset => scrollTop < offset - headerHeight);
+    return (sectionIdx >= 0)? sectionIdx : 3;
+  }
+
+  function activateMenuBtn(scrollTop) {
+    const activeIdx = currentSection(scrollTop) - 1;
+    if (activeIdx >= 0 && !menuBtnHasClass[activeIdx]) {
+      $menuBtn.removeClass('header-menuBtn--active');
+      $menuBtn.eq(activeIdx).addClass('header-menuBtn--active');
+      menuBtnHasClass.fill(false);
+      menuBtnHasClass[activeIdx] = true;
+      classesDeleted = false;
+    } else if (activeIdx < 0 && !classesDeleted) {
+      $menuBtn.removeClass('header-menuBtn--active');
+      menuBtnHasClass[0] = false;
+      classesDeleted = true;
+    }
+  }
+
+  // --- Header shadow  ---
 
   const $window = $(window);
   const $header = $('.header');
   let scrollTop = $window.scrollTop();
-  let ticking, hasClass;
+  let hasClass;
 
   if (scrollTop) {
     $header.addClass('header--shadow');
     hasClass = true;
   }
 
+  function headerShadow(scrollTop) {
+    if (scrollTop && !hasClass) {
+      $header.addClass('header--shadow');
+      hasClass = true;
+    } else if (!scrollTop && hasClass) {
+      $header.removeClass('header--shadow');
+      hasClass = false;
+    }
+  }
+
+  // --- Scroll event listener  ---
+
+  let ticking;
+
   $window.on('scroll', function() {
     if (!ticking) {
       scrollTop = $window.scrollTop();
       requestAnimationFrame(function() {
-        if (scrollTop && !hasClass) {
-          $header.addClass('header--shadow');
-          hasClass = true;
-        } else if (!scrollTop && hasClass) {
-          $header.removeClass('header--shadow');
-          hasClass = false;
-        }
+        headerShadow(scrollTop);
+        activateMenuBtn(scrollTop);
         ticking = false;
       });
     }
@@ -48,9 +108,22 @@ jQuery(document).ready(function($){
     $(this).toggleClass('jumbo-playBtn--paused');
   });
 
-  // ------------ Services -- Modal elements ------------
+  // ------------ Services -- horizontal dotted line ------------
 
   const $servicesItem = $('.services-item');
+
+  if (!isMobile) {
+    new Tether({
+      element: $('.services-horizontalDottedLine'),
+      target: $servicesItem.eq(0),
+      attachment: 'top middle',
+      targetAttachment: 'bottom right',
+      offset: '0 1%'
+    });
+  }
+
+  // ------------ Services -- Modal elements ------------
+
   const $servicesModal = $('.services-modal');
 
   $servicesItem.each((idx, $item) => {
@@ -70,6 +143,7 @@ jQuery(document).ready(function($){
   $servicesItem.click(function() {
     const thisIdx = $servicesItem.index(this);
     const $thisModal = $servicesModal.eq(thisIdx);
+    $(this).toggleClass('services-item--active');
     if ($thisModal.is(':visible')) {
       $thisModal.hide(250);
       return;
@@ -77,7 +151,6 @@ jQuery(document).ready(function($){
     $servicesModal.hide();
     $thisModal.show(250);
   });
-
 
   // ------------ Contact -- Dropdown ------------
 
@@ -108,7 +181,8 @@ jQuery(document).ready(function($){
     $this.next('.contact-selectDefinition').toggleClass('contact-selectDefinition--checked');
   });
   
-  $contactSelectDefinition.click(function() {
+  $contactSelectDefinition.click(function(e) {
+    e.preventDefault();
     const $this = $(this);
     const $select = $this.prev('.contact-select');
     $select.find('.contact-selectInput')[0].checked = !$select.find('.contact-selectInput')[0].checked;
